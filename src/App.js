6952b2cc4f9c51ed1bf8a9c2ca63837282be6500
Aakash S <aakashshinde67@gmail.com>
2023-02-16 11:31:25 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, Aakash shinde
         </p>
         <a
           className="App-link"
