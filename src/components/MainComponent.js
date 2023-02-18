fb6ff1f502e4062d5e691028c989703e7e03bff6
Aakash S <aakashshinde67@gmail.com>
2023-02-18 20:17:45 +0530

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,7 +11,7 @@ import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 
 
-import { Switch, Router, Redirect, Route } from 'react-router-dom';
+import { Switch,  Redirect, Route } from 'react-router-dom';
 
 
 class Main extends Component {
@@ -26,7 +26,7 @@ class Main extends Component {
     }
 
     render() {
-        const HomePage=()=> {
+        const HomePage = () => {
             return(
             <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]} 
             promotion={this.state.promotions.filter((promo)=>promo.featured)[0]} 
@@ -34,12 +34,22 @@ class Main extends Component {
             />
             );
         }
+
+        const DishWithId=({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish)=>dish.id === parseInt(match.params.dishId,10))[0]}
+            comments={this.state.comments.filter((comment)=>comment.dishId === parseInt(match.params.dishId,10))}
+            />
+                );
+        };
+     
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
 
